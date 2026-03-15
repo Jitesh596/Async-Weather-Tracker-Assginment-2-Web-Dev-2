ec8317535caf4cb51191325f998366f953ff310d
Jitesh596 <jtskumar05@gmail.com>
2026-03-15 19:42:18 +0530

    15-03-26(PM)

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const form = document.querySelector('#weather-form');
 const weatherInfo = document.querySelector("#weatherBox");
 const cityInput = document.querySelector("#city");
 const history = document.querySelector("#history");
-const consoleBox = document.querySelector("#consoleBox");
+
 
 let historydata = JSON.parse(localStorage.getItem("history")) || [];
 
@@ -76,7 +76,3 @@ function showHistory() {
 showHistory();
 
 
-function log(message) {
-    consoleBox.innerHTML += message + "<br>";
-    consoleBox.scrollTop = consoleBox.scrollHeight;
-}
